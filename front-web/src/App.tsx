b24bd468481da69d3ddbdf1c28e4807cbcd161b7
SuayMack <pry_chu@hotmail.com>
2021-01-09 14:49:21 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
   return (
     <div>
-      <Navbar/>
+      <Navbar />
+      <Home />
     </div>
   );
 }
